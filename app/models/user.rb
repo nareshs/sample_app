eaa896ad86bd2cb9819ab0baa8c5c6aad784dad7
Naresh <naresh@youhaveiwant.com>
2010-12-06 17:23:36 -0500

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ attr_accessible :name, :email, :password, :password_confirmation
 
 email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-validates :name, :presence => true,
+validates :name, :presence => true,	
 				 :length => {:maximum => 50}
 validates :email, :presence => true,
 		  :format   => { :with => email_regex },
